9532ac5f55c4eda2c0b3ba6f0af8ee60918e8cf1
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2022-11-13 14:18:34 +0900

    init

---
+++ src/Utf16FastCompression.MemoryPack/FastStringFormatter.cs
@@ -0,0 +1,17 @@
+﻿using MemoryPack;
+using System.Buffers;
+
+namespace Utf16FastCompression.MemoryPack;
+
+public sealed class FastStringFormatter : IMemoryPackFormatter<string>
+{
+    public void Deserialize(scoped ref MemoryPackReader reader, scoped ref string? value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Serialize<TBufferWriter>(scoped ref MemoryPackWriter<TBufferWriter> writer, scoped ref string? value) where TBufferWriter : IBufferWriter<byte>
+    {
+        throw new NotImplementedException();
+    }
+}
